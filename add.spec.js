62e436d73a115f6362e6687a9c2fe15453b8f897
LucianoBuhler <lucianobuhler@gmail.com>
2025-09-19 16:10:02 -0300

    Added Mocha testing framework

--- add.spec.js
+++
@@ -1,8 +0,0 @@
-const add = require('./add');
-const assert = require('assert');
-
-const result = add (1, 3);
-
-const expectedResult = 4
-
-assert.equal(result, expectedResult, `Add function does not working properly: expected 1 + 3 to equal 4 but got ${result}`);
---
+++ test/add.spec.js
@@ -0,0 +1,13 @@
+const add = require('../add');
+const assert = require('assert');
+
+describe('The add function', () => {
+  it('should correctly add two numbers', () => {
+    const actual = add(1, 3);
+
+    const expected = 4;
+
+    assert.equal(actual, expected);
+  });
+})
+
